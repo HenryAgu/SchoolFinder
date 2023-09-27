22d0f89bee0ecb46dcdf4c7a24487b38262b9c15
Henry Agu <aguhenrychuks@gmail.com>
2023-09-27 15:22:01 +0100

    feat: register page updated

--- src/components/Navbar/SignInModal.jsx
+++ src/components/Navbar/SignInModal.jsx
@@ -7,6 +7,7 @@ import CancelIcon from "./images/cancel.svg";
 import EmailIcon from "./images/email.svg";
 import PasswordIcon from "./images/password.svg";
 import Google from "./images/Google.svg";
+import { NavLink } from "react-router-dom";
 
 const SignInModal = ({ setOpenSignIn }) => {
   return (
@@ -23,14 +24,14 @@ const SignInModal = ({ setOpenSignIn }) => {
           <img src={Logo} alt="Logo" />
           <p>Welcome Back!</p>
         </div>
-        <form>
+        <form className="sign_in_form">
           <div className="inner_form">
             <img src={EmailIcon} alt="Email" />
-            <input type="email" placeholder="Gmail" autocomplete="off"/>
+            <input type="email" placeholder="Gmail" autocomplete="off" />
           </div>
           <div className="inner_form">
             <img src={PasswordIcon} alt="Password" />
-            <input type="password" placeholder="Password" autocomplete="off"/>
+            <input type="password" placeholder="Password" autocomplete="off" />
           </div>
           <button>Login</button>
         </form>
@@ -39,10 +40,12 @@ const SignInModal = ({ setOpenSignIn }) => {
           <span>or</span>
           <hr></hr>
         </div>
+        <NavLink to="/register">
           <button className="google_login">
-          <img src={Google} alt="Google" />
-          <p>Register with Gmail</p>
+            <img src={Google} alt="Google" />
+            <p>Register with Gmail</p>
           </button>
+        </NavLink>
       </div>
     </div>
   );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+
+// stylesheet
+import "./style/Register.css";
+
+// images
+import Ecllipse from "./images/ecllipse.png";
+import Logo from "../../components/Navbar/images/Logo.svg";
+import School from "./images/school.svg";
+import Gmail from "./images/gmail.svg";
+import Phone from "./images/phone.svg";
+import Password from "./images/password.svg";
+import Website from "./images/website.svg";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <section className="register">
+      <div className="register_container">
+        <img src={Ecllipse} alt="Ecllipse" className="ecllipse"/>
+        <div className="register_header">
+          <img src={Logo} alt="Logo" />
+          <h1>Register your school</h1>
+        </div>
+        <form className="register_form">
+          <div className="inner_form">
+            <div className="input_content">
+              <img src={School} alt="school" />
+              <input type="text" placeholder="School Name" />
+            </div>
+            <div className="input_content">
+              <input type="text" placeholder="I’m the School.." />
+            </div>
+          </div>
+          <div className="inner_form">
+            <div className="input_content">
+              <img src={Gmail} alt="Gmail" />
+              <input type="email" placeholder="Gmail" />
+            </div>
+            <div className="input_content">
+              <img src={Phone} alt="Phone" />
+              <input type="text" placeholder="Phone Number" />
+            </div>
+          </div>
+          <div className="inner_form">
+            <div className="input_content">
+              <img src={Password} alt="Password" />
+              <input type="password" placeholder="Create Password" />
+            </div>
+            <div className="input_content">
+              <img src={Password} alt="Password" />
+              <input type="password" placeholder="Confirm Password" />
+            </div>
+          </div>
+          <div className="school_website">
+            <img src={Website} alt="image" />
+            <input type="text" placeholder="School Website" />
+          </div>
+          <div className="upload_cert">
+            <input type="file" name="" id="" />
+            <p><span>Uplaod </span>School Registration Certification/License Doc</p>
+          </div>
+          <button>Create School Account</button>
+        </form>
+        <p className="login_route">Already have an account on SchoolLocator? <span>Login</span></p>
+      </div>
+    </section>
+  );
+};
 
-export default Register
+export default Register;
