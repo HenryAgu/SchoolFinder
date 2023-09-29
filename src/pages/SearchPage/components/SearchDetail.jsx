3f57492a49089f827124945c56f85d656b75fded
Henry Agu <aguhenrychuks@gmail.com>
2023-09-29 14:12:03 +0100

    feat: search page updated

--- src/pages/SearchPage/components/SearchDetail.jsx
+++ src/pages/SearchPage/components/SearchDetail.jsx
@@ -6,6 +6,12 @@ import "../style/SearchDetail.css";
 // images
 import CancelIcon from "../images/Cancel.svg";
 import RightArrow from "../images/RightArrow.svg";
+import SchoolImage from "../images/SchoolPhoto.png";
+import LikeIcon from "../images/Like.svg";
+import UnlikeIcon from "../images/Unlike.svg";
+import Rate from "../images/Rate.svg";
+import Location from "../images/Location.svg";
+import Price from "../images/Price.svg";
 
 const SearchDetail = () => {
   const levels = [
@@ -20,6 +26,32 @@ const SearchDetail = () => {
     {
       id: 3,
       level: "Secondary",
+    },
+  ];
+  const SchoolsDetail = [
+    {
+      id: 1,
+      SchoolName: "Glowfield School",
+      SchoolImage: SchoolImage,
+      Price: 200,
+      Location: "Calle Sin National Headquater",
+      SchoolType: "Private",
+    },
+    {
+      id: 2,
+      SchoolName: "Glowfield School",
+      SchoolImage: SchoolImage,
+      Price: 200,
+      Location: "Calle Sin National Headquater",
+      SchoolType: "Private",
+    },
+    {
+      id: 3,
+      SchoolName: "Glowfield School",
+      SchoolImage: SchoolImage,
+      Price: 200,
+      Location: "Calle Sin National Headquater",
+      SchoolType: "Private",
     },
   ];
   return (
@@ -46,9 +78,73 @@ const SearchDetail = () => {
             <img src={RightArrow} alt="Right Arrow" />
           </div>
         </div>
+        <div className="price">
+          <h5>Price, N</h5>
+          <div className="min_max">
+            <input type="text" placeholder="Min" />
+            <p>-</p>
+            <input type="text" placeholder="Max" />
+          </div>
+          <div className="price_range">
+            <div className="inner_range">
+              <input type="checkbox" name="" id="" />
+              <span>Under 50k</span>
+            </div>
+            <div className="inner_range">
+              <input type="checkbox" name="" id="" />
+              <span>50k - 100k</span>
+            </div>
+            <div className="inner_range">
+              <input type="checkbox" name="" id="" />
+              <span>100k - 200k</span>
+            </div>
+          </div>
+        </div>
+        <button>Apply Filter</button>
       </div>
-      <div className="school_list">2</div>
-      <div className="map">3</div>
+      <div className="school_list">
+        <div className="school_list_header">
+          <h1>Schools in Choba, Nigeria</h1>
+          <div className="list_header_info">
+            <p>29 Physical schools</p>
+            <p>10 Online schools</p>
+          </div>
+        </div>
+        <div className="school_list_container">
+          {SchoolsDetail.map((school) => (
+            <div className="school_card" key={school.id}>
+              <div className="left_school_card">
+                <img src={school.SchoolImage} alt="SchoolImage" />
+              </div>
+              <div className="right_school_card">
+                <div className="school_card_top">
+                  <h4>{school.SchoolName}</h4>
+                  <img src={LikeIcon} alt="icon" />
+                </div>
+                <div className="inner_school_info">
+                  <img src={Price} alt="Price" />
+                  <span>N{school.Price}K</span>
+                </div>
+                <div className="inner_school_info">
+                  <img src={Location} alt="location" />
+                  <p>{school.Location}</p>
+                </div>
+                <img src={Rate} alt="rate" />
+                <div className="school_card_bottom">
+                  <div className="left_card_bottom">
+                    <div className="rate_fig">4.2</div>
+                    <p>{school.SchoolType} School</p>
+                  </div>
+                  <button>Details</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="map">
+        
+      </div>
     </div>
   );
 };
